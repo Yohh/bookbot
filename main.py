af4eae423b617bb3c14697b3ec4ebde74e96e50b
Yohh <durandyohan@zaclys.net>
2025-07-16 22:31:35 +0200

    use sys in main

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import sys
 from stats import get_book_text_words, get_book_stats, sort_list
 
 
 def main():
-    filepath = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    filepath = sys.argv[1]
     num_words = len(get_book_text_words(filepath))
     char_count = get_book_stats(filepath)
     char_count.sort(key=sort_list, reverse=True)
