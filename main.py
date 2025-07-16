b3b54e95dec84d3d3c3b39259214fdf72496401b
Yohh <durandyohan@zaclys.net>
2025-07-16 21:59:19 +0200

    create get_book_stats, use it in main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_book_text_words
+from stats import get_book_stats
 
 
 def get_book_text(filepath):
@@ -10,7 +11,9 @@ def main():
     filepath = "books/frankenstein.txt"
     text = get_book_text(filepath)
     num_words = len(get_book_text_words(filepath))
+    char_count = get_book_stats(filepath)
     print(f"{num_words} words found in the document")
+    print(char_count)
 
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,15 @@ def get_book_text_words(filepath):
     with open(filepath) as f:
         return f.read().split()
 
+
+def get_book_stats(filepath):
+    with open(filepath) as f:
+        text = f.read()
+    char_count = {}
+    for char in text:
+        char = char.lower()
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
