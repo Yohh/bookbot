2710a91e0804b83a1e8ca392cece87964bb6d3ec
Yohh <durandyohan@zaclys.net>
2025-07-16 21:42:03 +0200

    setup main, add get_book_text function

---
+++ main.py
@@ -0,0 +1,10 @@
+def get_book_text(filepath):
+    with open(filepath) as f:
+        return f.read()
+
+def main():
+    filepath = 'books/frankenstein.txt'
+    text = get_book_text(filepath)
+    print(text)
+
+main()
