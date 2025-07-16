78cba53f98a61dc523f482b99bb539f13bebd3b9
Yohh <durandyohan@zaclys.net>
2025-07-16 21:47:29 +0200

    add get_book_text_words function

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
+
+def get_book_text_words(filepath):
+    with open(filepath) as f:
+        return f.read().split()
+
+
 def main():
-    filepath = 'books/frankenstein.txt'
+    filepath = "books/frankenstein.txt"
     text = get_book_text(filepath)
-    print(text)
+    num_words = len(get_book_text_words(filepath))
+    print(f"{num_words} words found in the document")
+
 
 main()
